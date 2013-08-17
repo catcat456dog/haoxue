ec75cb94a453e35af2d78f370c4b99a5210f7543
zhumaohua <zhumaohua@chinadrtv.com>
2013-08-17 20:37:14 +0800

    Neww

---
+++ ShowNow/ShowNow/Hibe.h
@@ -0,0 +1,13 @@
+//
+//  Hibe.h
+//  ShowNow
+//
+//  Created by zhumaohua on 13-8-17.
+//  Copyright (c) 2013年 zhumaohua. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Hibe : NSObject
+
+@end
---
+++ ShowNow/ShowNow/Hibe.m
@@ -0,0 +1,13 @@
+//
+//  Hibe.m
+//  ShowNow
+//
+//  Created by zhumaohua on 13-8-17.
+//  Copyright (c) 2013年 zhumaohua. All rights reserved.
+//
+
+#import "Hibe.h"
+
+@implementation Hibe
+
+@end
